f5bb33cf8968e6f95649eba9b2c78d4339543052
NajibHossain49 <najibhossain1234@gmail.com>
2024-12-13 00:12:04 +0600

    Get a single job and Apply a new job

--- index.js
+++ index.js
@@ -29,7 +29,8 @@ const client = new MongoClient(uri, {
 let collection;
 client.connect().then(() => {
   console.log("Connected to MongoDB!");
-  collection = client.db("echoglimmer").collection("Job-Collection"); 
+  collection = client.db("echoglimmer").collection("Job-Collection");
+  collection2 = client.db("echoglimmer").collection("JobApplication");
   // Replace with your actual DB name // Replace with your collection name
 });
 
@@ -41,34 +42,41 @@ async function run() {
     // Creates the Api endpoints from below.
 
     // Get all jobs
-    app.get("/jobs", async (req, res) => {
+    app.get("/LatestJobs", async (req, res) => {
       const jobs = await collection.find({}).toArray();
       res.send(jobs);
     });
 
-    // // Get a single job
-    // app.get("/jobs/:id", async (req, res) => {
-    //   const job = await collection.findOne({ _id: new ObjectId(req.params.id) });
-    //   res.send(job);
-    // });
+    // Get a single job
+    app.get("/job-details/:id", async (req, res) => {
+      const job = await collection.findOne({
+        _id: new ObjectId(req.params.id),
+      });
+      res.send(job);
+    });
 
-    // // Create a new job
-    // app.post("/jobs", async (req, res) => {
-    //   const job = req.body;
-    //   const result = await collection.insertOne(job);
-    //   res.send(result);
-    // });
+    // Apply a new job
+    app.post("/jobs", async (req, res) => {
+      const JobApplication = req.body;
+      const result = await collection2.insertOne(JobApplication);
+      res.send(result);
+    });
 
     // // Update a job
     // app.put("/jobs/:id", async (req, res) => {
     //   const job = req.body;
-    //   const result = await collection.updateOne({ _id: new ObjectId(req.params.id) }, { $set: job });
+    //   const result = await collection.updateOne(
+    //     { _id: new ObjectId(req.params.id) },
+    //     { $set: job }
+    //   );
     //   res.send(result);
     // });
 
     // // Delete a job
     // app.delete("/jobs/:id", async (req, res) => {
-    //   const result = await collection.deleteOne({ _id: new ObjectId(req.params.id) });
+    //   const result = await collection.deleteOne({
+    //     _id: new ObjectId(req.params.id),
+    //   });
     //   res.send(result);
     // });
 
@@ -86,17 +94,19 @@ async function run() {
 
     // // Search jobs by location
     // app.get("/jobs/search", async (req, res) => {
-    //   const jobs = await collection.find({ location: req.query.location }).toArray();
+    //   const jobs = await collection
+    //     .find({ location: req.query.location })
+    //     .toArray();
     //   res.send(jobs);
     // });
 
     // // Search jobs by company
     // app.get("/jobs/search", async (req, res) => {
-    //   const jobs = await collection.find({ company: req.query.company }).toArray();
+    //   const jobs = await collection
+    //     .find({ company: req.query.company })
+    //     .toArray();
     //   res.send(jobs);
     // });
-
-
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
   }
